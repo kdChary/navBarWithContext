8a8088e3637d0248055bac1cc7c5e49a5e78c465
Siva Dinesh k <ksd.ccbp4@gmail.com>
2024-03-06 12:34:36 +0000

    home route coded

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,5 +1,6 @@
 import './index.css'
 import ThemeContext from '../../context/ThemeContext'
+import Navbar from '../Navbar'
 
 const Home = () => (
   <ThemeContext.Consumer>
@@ -9,7 +10,19 @@ const Home = () => (
       const theme = isDarkTheme ? 'dark-theme' : 'light'
       const textColor = isDarkTheme ? 'dark-theme-text' : ''
 
-      return <div className="home-container">Hello</div>
+      const homeImgUrl = isDarkTheme
+        ? 'https://assets.ccbp.in/frontend/react-js/home-light-img.png'
+        : 'https://assets.ccbp.in/frontend/react-js/home-dark-img.png'
+
+      return (
+        <div className={`home-container ${theme}`}>
+          <Navbar />
+          <div className="home-card">
+            <img src={homeImgUrl} alt="home icon" className="main-icon" />
+            <h1 className={`main-heading ${textColor}`}>Home</h1>
+          </div>
+        </div>
+      )
     }}
   </ThemeContext.Consumer>
 )
