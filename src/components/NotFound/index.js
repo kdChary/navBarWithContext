401239e89418bbd993aac9a2c7dd99d77cf00e56
Siva Dinesh k <ksd.ccbp4@gmail.com>
2024-03-06 11:04:45 +0000

    not-found route coded

--- src/components/NotFound/index.js
+++ src/components/NotFound/index.js
@@ -1,1 +1,30 @@
-// Write your code here
+import ThemeContext from '../../context/ThemeContext'
+import './index.css'
+
+const NotFound = () => (
+  <ThemeContext.Consumer>
+    {value => {
+      const {isDarkTheme} = value
+
+      const theme = isDarkTheme ? 'dark-theme' : 'light'
+
+      return (
+        <div className={`not-found-container ${theme}`}>
+          <div className="not-found-card">
+            <img
+              src="https://assets.ccbp.in/frontend/react-js/not-found-img.png"
+              alt="not found"
+              className="not-found-image"
+            />
+            <h2 className="not-found-title">Lost Your Way ?</h2>
+            <p className="not-found-text">
+              We cannot seem to find the page you are looking for.
+            </p>
+          </div>
+        </div>
+      )
+    }}
+  </ThemeContext.Consumer>
+)
+
+export default NotFound
