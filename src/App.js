a156eb89d2c4f9835058d1c180b222df6a50da6d
Siva Dinesh k <ksd.ccbp4@gmail.com>
2024-03-06 10:49:12 +0000

    provider added to App.js

--- src/App.js
+++ src/App.js
@@ -3,21 +3,22 @@ import {Switch, Route} from 'react-router-dom'
 import {Component} from 'react'
 
 import Home from './components/Home'
-
 import About from './components/About'
-
 import NotFound from './components/NotFound'
+import ThemeContext from './context/ThemeContext'
 
 import './App.css'
 
 class App extends Component {
   render() {
     return (
-      <Switch>
-        <Route exact path="/" component={Home} />
-        <Route exact path="/about" component={About} />
-        <Route component={NotFound} />
-      </Switch>
+      <ThemeContext.Provider>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/about" component={About} />
+          <Route component={NotFound} />
+        </Switch>
+      </ThemeContext.Provider>
     )
   }
 }
