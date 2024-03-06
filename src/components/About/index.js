5d4f14253a977ff134b085092cb1fd6ff7c8739d
Siva Dinesh k <ksd.ccbp4@gmail.com>
2024-03-06 12:43:30 +0000

    about component added and styled using existing css

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,1 +1,30 @@
-// Write your code here
+import './index.css'
+import ThemeContext from '../../context/ThemeContext'
+import Navbar from '../Navbar'
+
+const About = () => (
+  <ThemeContext.Consumer>
+    {value => {
+      const {isDarkTheme} = value
+
+      const theme = isDarkTheme ? 'dark-theme' : 'light'
+      const textColor = isDarkTheme ? 'dark-theme-text' : ''
+
+      const aboutImgUrl = !isDarkTheme
+        ? 'https://assets.ccbp.in/frontend/react-js/about-light-img.png'
+        : 'https://assets.ccbp.in/frontend/react-js/about-dark-img.png'
+
+      return (
+        <div className={`home-container ${theme}`}>
+          <Navbar />
+          <div className="home-card">
+            <img src={aboutImgUrl} alt="about icon" className="main-icon" />
+            <h1 className={`main-heading ${textColor}`}>About</h1>
+          </div>
+        </div>
+      )
+    }}
+  </ThemeContext.Consumer>
+)
+
+export default About
